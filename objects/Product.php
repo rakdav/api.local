a2d14df882020c58080ca1c4b2575325e602ef83
rakdav <rakdav@inbox.ru>
2025-11-22 12:59:52 +0200

    3

--- objects/Product.php
+++ objects/Product.php
@@ -30,7 +30,21 @@ class Product
     function create()
     {
         $query = "INSERT INTO $this->table (name,description,price,created,category_id)
-values (:name,:description,:price,:created,:category_id)";
-
+        values (:name,:description,:price,:created,:category_id)";
+        $stmt = $this->conn->prepare($query);
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->description = htmlspecialchars(strip_tags($this->description));
+        $this->price = htmlspecialchars(strip_tags($this->price));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+        $this->created = htmlspecialchars(strip_tags($this->created));
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":description", $this->description);
+        $stmt->bindParam(":price", $this->price);
+        $stmt->bindParam(":category_id", $this->category_id);
+        $stmt->bindParam(":created", $this->created);
+        if($stmt->execute()){
+            return true;
+        }
+        return false;
     }
 }
